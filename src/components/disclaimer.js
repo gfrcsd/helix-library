f7cc31873b761b312f1d97a6a79c8c4d538c3608
gfrcsd <23112101+gfrcsd@users.noreply.github.com>
2020-04-25 19:23:24 +0100

    Add registered unicode to Line 6 brand

--- src/components/disclaimer.js
+++ src/components/disclaimer.js
@@ -4,7 +4,7 @@ const Disclaimer = () => {
     return(
         <div className="hero is-small is-warning">
             <div className="hero-body has-text-centered">
-                <p><b className="has-text-black">Disclaimer:</b> this website has no affiliation with Line 6 or any of the companies mentionned. For official documentation and support, visit <a className="has-text-link" href="https://line6.com/helix/">Line 6</a></p>
+                <p><b className="has-text-black">Disclaimer:</b> this website has no affiliation with Line 6® or any of the companies mentionned. For official documentation and support, visit <a className="has-text-link" href="https://line6.com/helix/">Line 6</a></p>
             </div>
         </div>
     )
--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ const IndexPage = () => (
           <div className="columns is-centered">
             <div className="column is-10">
               <h1 className="title">Welcome to Helix library</h1>
-              <h2 className="subtitle">A trove of informations about the Helix product line by Line 6</h2>
+              <h2 className="subtitle">A trove of informations about the Helix product line by Line 6®</h2>
             </div>
           </div>
         </div>
