0fbcd55515eb8c60f6eab49a82e994c85831a833
gfrcsd <23112101+gfrcsd@users.noreply.github.com>
2020-06-07 21:24:29 +0100

    Add title and link for manuals

--- src/components/disclaimer.js
+++ src/components/disclaimer.js
@@ -1,10 +1,12 @@
 import React from "react"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faExternalLink } from '@fortawesome/pro-duotone-svg-icons'
 
 const Disclaimer = () => {
     return(
         <div className="hero is-small is-warning">
             <div className="hero-body has-text-centered">
-                <p><b className="has-text-black">Disclaimer:</b> this website has no affiliation with Line 6® or any of the companies mentioned. For official documentation and support, visit <a className="has-text-link" href="https://line6.com/helix/">Line 6</a></p>
+                <p><b className="has-text-black">Disclaimer:</b> this website has no affiliation with Line 6® or any of the companies mentioned. For official documentation and support, visit <a className="has-text-link" href="https://line6.com/helix/">Line 6 <FontAwesomeIcon icon={faExternalLink} fixedWidth /></a></p>
             </div>
         </div>
     )
--- src/pages/manuals.js
+++ src/pages/manuals.js
@@ -5,13 +5,41 @@ import Section from "../components/section"
 import Heading from "../components/heading"
 import Disclaimer from "../components/disclaimer"
 import "../sass/style.sass"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faExternalLink } from '@fortawesome/pro-duotone-svg-icons'
+
 
 const ManualsPage = () => (
   <Layout>
     <SEO title="Manuals" />
     <Heading title="Manuals" subtitle="Download the official documentation" color="light" size="normal"/>
     <Section>
-
+      <div className="columns is-multiline">
+        <div className="column is-one-third has-text-centered">
+          <h2 className="is-size-4">Helix</h2>
+          <a href="https://line6.com/support/manuals/helix/">Line 6 Official <FontAwesomeIcon icon={faExternalLink} fixedWidth /></a>
+        </div>
+        <div className="column is-one-third has-text-centered">
+          <h2 className="is-size-4">Helix Rack</h2>
+          <a href="https://line6.com/support/manuals/helix-rack/">Line 6 Official <FontAwesomeIcon icon={faExternalLink} fixedWidth /></a>
+        </div>
+        <div className="column is-one-third has-text-centered">
+          <h2 className="is-size-4">Helix Native</h2>
+          <a href="https://line6.com/support/manuals/helix-native/">Line 6 Official <FontAwesomeIcon icon={faExternalLink} fixedWidth /></a>
+        </div>
+        <div className="column is-one-third has-text-centered">
+          <h2 className="is-size-4">Helix LT</h2>
+          <a href="https://line6.com/support/manuals/helixlt/">Line 6 Official <FontAwesomeIcon icon={faExternalLink} fixedWidth /></a>
+        </div>
+        <div className="column is-one-third has-text-centered">
+          <h2 className="is-size-4">Helix Effects</h2>
+          <a href="https://line6.com/support/manuals/hxeffects/">Line 6 Official <FontAwesomeIcon icon={faExternalLink} fixedWidth /></a>
+        </div>
+        <div className="column is-one-third has-text-centered">
+          <h2 className="is-size-4">Helix Stomp</h2>
+          <a href="https://line6.com/support/manuals/hxstomp/">Line 6 Official <FontAwesomeIcon icon={faExternalLink} fixedWidth /></a>
+        </div>
+      </div>
     </Section>
     <Disclaimer/>
   </Layout>
