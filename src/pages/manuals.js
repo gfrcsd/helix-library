d48667be5a3c5a806a56d9f42bb65f2423cd602f
gfrcsd <23112101+gfrcsd@users.noreply.github.com>
2020-06-09 13:57:40 +0100

    Adjust container width on manuals page

--- src/pages/manuals.js
+++ src/pages/manuals.js
@@ -15,14 +15,18 @@ export default ({data}) => {
       <SEO title="Manuals" />
       <Heading title="Manuals" subtitle="Download the official documentation" color="light" size="normal"/>
       <Section>
-        <div className="columns is-multiline">
-          {data.allMarkdownRemark.edges.map(({ node }) => (
-            <div className="column is-4 has-text-centered">
-              <Img fluid={node.frontmatter.image.childImageSharp.fluid}/>
-              <h2 className="is-size-4">{node.frontmatter.name}</h2>
-              <a href={node.frontmatter.url}>Line 6 Official <FontAwesomeIcon icon={faExternalLink} fixedWidth /></a>
+        <div className="columns is-centered is-multiline">
+          <div className="column is-10">
+            <div className="columns is-centered is-multiline">
+              {data.allMarkdownRemark.edges.map(({ node }) => (
+                <div className="column is-4 has-text-centered">
+                  <Img fluid={node.frontmatter.image.childImageSharp.fluid}/>
+                  <h2 className="is-size-4">{node.frontmatter.name}</h2>
+                  <a href={node.frontmatter.url}>Line 6 Official <FontAwesomeIcon icon={faExternalLink} fixedWidth /></a>
+                </div>
+              ))}
             </div>
-          ))}
+          </div>
         </div>
       </Section>
       <Disclaimer/>
