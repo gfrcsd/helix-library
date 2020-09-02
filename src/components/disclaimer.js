5b42eb578824534b2ea531b15288d61ca75fde52
gfrcsd <23112101+gfrcsd@users.noreply.github.com>
2020-09-02 18:59:47 +0100

    Change disclaimer font size on mobile

--- src/components/disclaimer.js
+++ src/components/disclaimer.js
@@ -5,7 +5,7 @@ import { faExternalLink } from '@fortawesome/pro-duotone-svg-icons'
 const Disclaimer = () => {
     return(
         <div className="hero is-small is-warning">
-            <div className="hero-body has-text-centered">
+            <div className="hero-body has-text-centered is-size-7-mobile">
                 <p><b className="has-text-black">Disclaimer:</b> this website has no affiliation with Line 6® or any of the companies mentioned. For official documentation and support, visit <a className="has-text-link" href="https://line6.com/helix/">Line 6 <FontAwesomeIcon icon={faExternalLink} fixedWidth /></a></p>
             </div>
         </div>
