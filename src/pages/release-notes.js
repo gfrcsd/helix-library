3a1c136cad7b3e476d9d73603277062f4f50a048
gfrcsd <23112101+gfrcsd@users.noreply.github.com>
2020-09-03 01:07:38 +0100

    Hide some element on mobile on the release note page

--- src/pages/release-notes.js
+++ src/pages/release-notes.js
@@ -1,5 +1,7 @@
 import React from "react"
 import { graphql, Link } from "gatsby"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronDown } from '@fortawesome/pro-duotone-svg-icons'
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Disclaimer from "../components/disclaimer"
@@ -7,13 +9,14 @@ import Section from "../components/section"
 import Heading from "../components/heading"
 
 export default ({data}) => {
+    const [isActive, setisActive] = React.useState(false)
     return(
         <Layout>
             <SEO title="Release Notes" description="Release notes for all the updates available for the family of Helix products"/>
             <Heading title="Release Notes" subtitle={data.allMdx.totalCount + ' published'} color="light" size="normal"/>
             <Section>
                 <div className="release-note columns is-centered">
-                    <div className="column is-2">
+                    <div className="column is-2 is-hidden-mobile">
                         <aside className="menu">
                             <ul className="menu-list">
                                 <p className="menu-label">Navigation</p>
@@ -31,12 +34,12 @@ export default ({data}) => {
                             <div id={node.frontmatter.title} className="column is-10 content headings">
                                 <h3 class="title is-size-4">Version {node.frontmatter.title}</h3>
                                 <h4 class="subtitle is-size-6 has-text-weight-normal">released on {node.frontmatter.date}</h4>
-                                <ul>
+                                <ul className="is-hidden-mobile">
                                 {node.headings.map(({ value }) => (
                                     <li>{value}</li>
                                 ))}
                                 </ul>
-                                <p>{node.excerpt}</p>
+                                <p className="is-hidden-mobile">{node.excerpt}</p>
                                 <Link style={{selfAlign:'right'}} className="button is-info" to={node.frontmatter.path}>Read more</Link>
                                 <hr/>
                             </div>
