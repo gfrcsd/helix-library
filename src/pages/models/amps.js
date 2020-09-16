4ffb736e9e2c379c46a510077979590d7ca8131a
gfrcsd <23112101+gfrcsd@users.noreply.github.com>
2020-09-16 14:09:22 +0100

    Add instrument filtering on amp page

--- src/pages/models/amps.js
+++ src/pages/models/amps.js
@@ -9,11 +9,45 @@ import Disclaimer from "../../components/disclaimer"
 import Table from "../../components/table"
 
 export default ({data}) => {
+
+    var all = document.getElementsByClassName('table-row');
+    var guitar = document.getElementsByClassName('guitar');
+    var bass = document.getElementsByClassName('bass');
+
+    function hide(row){
+        for (var i = 0; i < row.length; i ++) {
+            row[i].style.display = 'none';
+        }
+    }
+
+    function show(row){
+        for (var i = 0; i < row.length; i ++) {
+            row[i].style.display = 'table-row';
+        }
+    }
+
+    function toggle(item){
+        hide(all);
+        show(item);
+    }
+    
     return (
         <Layout>
             <SEO title="Amps" description="Find all the informations you need about the amps available in the Helix products by Line 6&#174;"/>
             <Heading title="Amps" subtitle={ data.allMarkdownRemark.totalCount + " available"} color="dark" size="medium"/>
             <Section>
+                <div className="columns">
+                    <div className="column is-narrow is-offset-1">
+                        <div className="buttons">
+                            <p className="filter-tag">Instrument:</p>
+                            <div className="buttons has-addons" id="instrumentButtons">
+                                <buttons onClick={() => toggle(all)} className="button is-small is-active">All</buttons>
+                                <buttons onClick={() => toggle(guitar)} className="button is-small">Guitar</buttons>
+                                <buttons onClick={() => toggle(bass)} className="button is-small">Bass</buttons>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 <Table narrow="true">
                     <thead>
                         <tr>
@@ -27,7 +61,7 @@ export default ({data}) => {
                     </thead>
                     <tfoot>
                     {data.allMarkdownRemark.edges.map(({ node }) => (
-                        <tr>
+                        <tr className={'table-row ' + node.frontmatter.instrument.toString().toLowerCase()}>
                             <td><Img fixed={node.frontmatter.icon.childImageSharp.fixed}/></td>
                             <td>{node.frontmatter.name}</td>
                             <td>{node.frontmatter.brand}&#174; {node.frontmatter.model}</td>
