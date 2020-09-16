dac3104b3e5f9b1076cf2b87f9080dc8a0203afa
gfrcsd <23112101+gfrcsd@users.noreply.github.com>
2020-09-16 19:06:30 +0100

    Change columns sizing for desktop filter button container

--- src/components/filterButtonContainer.js
+++ src/components/filterButtonContainer.js
@@ -5,28 +5,28 @@ import { faFilter } from '@fortawesome/pro-solid-svg-icons'
 export default ({filterName, children}) => {
     return (
         <div className="columns">
-        <div className="column is-narrow is-offset-1 is-hidden-mobile">
-            <div className="buttons">
-                <p className="filter-tag"><FontAwesomeIcon icon={faFilter} /> {filterName}</p>
-                <div className="buttons has-addons">
-                    {children}
+            <div className="column is-10 is-offset-1 is-hidden-mobile">
+                <div className="buttons">
+                    <p className="filter-tag"><FontAwesomeIcon icon={faFilter} /> {filterName}</p>
+                    <div className="buttons has-addons">
+                        {children}
+                    </div>
                 </div>
             </div>
-        </div>
-        <div className="column is-hidden-desktop">
-            <div className="columns">
-                <div className="column is-fullwidth is-narrow">
-                    <p className="filter-tag"><FontAwesomeIcon icon={faFilter} />Filter by {filterName}</p>
-                </div>
-                <div className="column">
-                    <div className="buttons">
-                        <div className="buttons has-addons">
-                            {children}
+            <div className="column is-hidden-desktop">
+                <div className="columns">
+                    <div className="column is-fullwidth is-narrow">
+                        <p className="filter-tag"><FontAwesomeIcon icon={faFilter} />Filter by {filterName}</p>
+                    </div>
+                    <div className="column">
+                        <div className="buttons">
+                            <div className="buttons has-addons">
+                                {children}
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
     )
 }
