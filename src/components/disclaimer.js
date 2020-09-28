fbbb28eb0225210965cfa3b4ce5928b21ddf6cbd
gfrcsd <23112101+gfrcsd@users.noreply.github.com>
2020-09-28 04:08:51 +0100

    Add missing character on disclaimer component

--- src/components/disclaimer.js
+++ src/components/disclaimer.js
@@ -6,7 +6,7 @@ const Disclaimer = () => {
     return(
         <div className="hero is-small is-warning">
             <div className="hero-body has-text-centered is-size-7-mobile">
-                <p><b className="has-text-weight-semibold has-text-black"><FontAwesomeIcon icon={faExclamationTriangle} fixedWidth /> Disclaimer </b> this website has no affiliation with Line 6® or any of the companies mentioned. For official documentation and support, visit <a className="has-text-link" href="https://line6.com/helix/">Line 6 <FontAwesomeIcon icon={faExternalLink} fixedWidth /></a></p>
+                <p><b className="has-text-weight-semibold has-text-black"><FontAwesomeIcon icon={faExclamationTriangle} fixedWidth /> Disclaimer: </b>this website has no affiliation with Line 6® or any of the companies mentioned. For official documentation and support, visit <a className="has-text-link" href="https://line6.com/helix/">Line 6 <FontAwesomeIcon icon={faExternalLink} fixedWidth /></a></p>
             </div>
         </div>
     )
