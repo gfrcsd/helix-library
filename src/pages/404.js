1da3348ad24f60c0ae269c4e5328cb25ada77cf1
gfrcsd <23112101+gfrcsd@users.noreply.github.com>
2020-10-01 23:40:14 +0100

    Revamp 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,19 @@
 import React from "react"
+import {Link} from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="Page not found" />
+      <div className="hero is-medium is-light is-bold">
+        <div className="hero-body has-text-centered">
+          <h1 className="title has-text-weight-normal has-text-weight-normal is-size-1">404</h1>
+          <h2 className="subtitle has-text-weight-light is-size-3 is-size-6-mobile">Page not found</h2>
+          <Link to="/" className="button">Back to homepage</Link>
+        </div>
+      </div>
   </Layout>
 )
 
