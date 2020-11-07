d9531b7adff8bf04de8bda0f40c6c35bcc477b3c
gfrcsd <23112101+gfrcsd@users.noreply.github.com>
2020-11-07 09:59:55 +0000

    Improve footer responsiveness for mobile device

--- src/components/footer.js
+++ src/components/footer.js
@@ -24,17 +24,17 @@ export default function Footer() {
     return(
         <footer className="footer">
             <div className="container">
-                <div className="columns">
-                    <div className="column is-3 has-text-grey has-text-weight-light">
+                <div className="columns is-mobile is-multiline">
+                    <div className="column is-3-desktop is-12-mobile has-text-grey has-text-weight-light">
                         <p><b className="has-text-grey-dark has-text-weight-normal">Helix Library</b> by <a href="https://github.com/gfrcsd">gfrcsd</a></p>
                         <p>Source code licensed <a href="https://opensource.org/licenses/gpl-3.0.html">GPL-3.0</a></p>
                         <p>Made with <a href="https://gatsbyjs.com/">Gatsby</a> & <a href="https://bulma.io">Bulma</a></p>
                         <p>Hosted on <a href="https://www.netlify.com/">Netlify</a></p>
                     </div>
-                    <div className="column is-1">
+                    <div className="column is-1-desktop is-12-mobile">
                         <Link className="has-text-dark" to="/">Home</Link>
                     </div>
-                    <div className="column">
+                    <div className="column is-6-mobile">
                         <p className="has-text-dark">Models</p>
                         <ul className="footer-list">
                             <li><Link to="/models/amps">Amps</Link></li>
@@ -43,7 +43,7 @@ export default function Footer() {
                             <li><Link to="/models/mics">Mics</Link></li>
                         </ul>
                     </div>
-                    <div className="column">
+                    <div className="column is-6-mobile">
                         <p className="has-text-dark">Manuals</p>
                         <ul className="footer-list">
                         {data.footerManuals.nodes.map(({ frontmatter }) => (
@@ -52,7 +52,7 @@ export default function Footer() {
                             <li><Link className="has-text-grey" to="/manuals">View all</Link></li>
                         </ul>
                     </div>
-                    <div className="column">
+                    <div className="column is-6-mobile">
                         <p className="has-text-dark" to="/release-notes">Release Notes</p>
                         <ul className="footer-list">
                         {data.footerReleasenotes.nodes.map(({ frontmatter }) => (
@@ -61,7 +61,7 @@ export default function Footer() {
                             <li><Link className="has-text-grey" to="/release-notes">View all</Link></li>
                         </ul>
                     </div>
-                    <div className="column has-text-weight-light">
+                    <div className="column is-6-mobile has-text-weight-light">
                         <p><b className="has-text-dark has-text-weight-normal">Contribute</b> on GitHub</p>
                         <iframe className="mt-1" src="https://ghbtns.com/github-btn.html?user=gfrcsd&repo=helix-library&type=star&count=false&v=2" frameborder="0" scrolling="0" width="150" height="20" title="GitHub"></iframe>
                     </div>
